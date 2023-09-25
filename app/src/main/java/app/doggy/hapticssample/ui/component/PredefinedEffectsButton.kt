3d98cc4671bc610f2ea619e1d295b26105edebc5
Kaito-Dogi <kendobu0405@gmail.com>
2023-09-25 21:20:09 +0900

    :recycle: EffectId アノテーションで渡せる整数を制限

--- app/src/main/java/app/doggy/hapticssample/ui/component/PredefinedEffectsButton.kt
+++ app/src/main/java/app/doggy/hapticssample/ui/component/PredefinedEffectsButton.kt
@@ -1,10 +1,14 @@
 package app.doggy.hapticssample.ui.component
 
-import android.annotation.SuppressLint
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.VibrationEffect.EFFECT_CLICK
+import android.os.VibrationEffect.EFFECT_DOUBLE_CLICK
+import android.os.VibrationEffect.EFFECT_HEAVY_CLICK
+import android.os.VibrationEffect.EFFECT_TICK
 import android.os.Vibrator
+import androidx.annotation.IntDef
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -13,10 +17,11 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.content.ContextCompat
 import app.doggy.hapticssample.ui.theme.HapticsSampleTheme
 
+@RequiresApi(Build.VERSION_CODES.Q)
 @Composable
 internal fun PredefinedEffectsButton(
   label: String,
-  effectId: Int,
+  @EffectId effectId: Int,
   vibrator: Vibrator,
   modifier: Modifier = Modifier,
 ) {
@@ -30,7 +35,18 @@ internal fun PredefinedEffectsButton(
   }
 }
 
-@SuppressLint("InlinedApi")
+@RequiresApi(Build.VERSION_CODES.Q)
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@IntDef(
+  EFFECT_TICK,
+  EFFECT_CLICK,
+  EFFECT_HEAVY_CLICK,
+  EFFECT_DOUBLE_CLICK,
+)
+private annotation class EffectId
+
+@RequiresApi(Build.VERSION_CODES.Q)
 @Preview
 @Composable
 private fun PredefinedEffectsButtonPreview() {

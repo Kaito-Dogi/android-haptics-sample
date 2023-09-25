f3c647b61a1ffbc3bf6dff8c0ea695d1ec994364
Kaito-Dogi <kendobu0405@gmail.com>
2023-09-26 00:31:24 +0900

    :recycle: 念のため Stable アノテーションを付与

--- app/src/main/java/app/doggy/hapticssample/ui/component/CompositionPrimitivesButton.kt
+++ app/src/main/java/app/doggy/hapticssample/ui/component/CompositionPrimitivesButton.kt
@@ -14,6 +14,7 @@ import androidx.annotation.IntRange
 import androidx.annotation.RequiresApi
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -46,6 +47,7 @@ internal fun CompositionPrimitivesButton(
   }
 }
 
+@Stable
 internal data class Primitive(
   @PrimitiveId val primitiveId: Int,
   @FloatRange(

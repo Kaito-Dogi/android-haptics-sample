0021d5cccd40ac40c901854ebd64f5fc50d302e4
Kaito-Dogi <kendobu0405@gmail.com>
2023-09-25 21:38:52 +0900

    :recycle: FeedBackConstant で渡せる整数を制限

--- app/src/main/java/app/doggy/hapticssample/ui/component/HapticFeedbackConstantsButton.kt
+++ app/src/main/java/app/doggy/hapticssample/ui/component/HapticFeedbackConstantsButton.kt
@@ -1,6 +1,20 @@
 package app.doggy.hapticssample.ui.component
 
+import android.os.Build
+import android.view.HapticFeedbackConstants.CLOCK_TICK
+import android.view.HapticFeedbackConstants.CONFIRM
+import android.view.HapticFeedbackConstants.CONTEXT_CLICK
+import android.view.HapticFeedbackConstants.GESTURE_END
+import android.view.HapticFeedbackConstants.GESTURE_START
+import android.view.HapticFeedbackConstants.KEYBOARD_PRESS
+import android.view.HapticFeedbackConstants.KEYBOARD_RELEASE
+import android.view.HapticFeedbackConstants.LONG_PRESS
+import android.view.HapticFeedbackConstants.REJECT
+import android.view.HapticFeedbackConstants.TEXT_HANDLE_MOVE
 import android.view.HapticFeedbackConstants.VIRTUAL_KEY
+import android.view.HapticFeedbackConstants.VIRTUAL_KEY_RELEASE
+import androidx.annotation.IntDef
+import androidx.annotation.RequiresApi
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -10,7 +24,7 @@ import app.doggy.hapticssample.ui.theme.HapticsSampleTheme
 @Composable
 internal fun HapticFeedbackConstantsButton(
   label: String,
-  feedBackConstant: Int,
+  @FeedBackConstant feedBackConstant: Int,
   modifier: Modifier = Modifier,
 ) {
   CustomButton(
@@ -20,6 +34,26 @@ internal fun HapticFeedbackConstantsButton(
     view.performHapticFeedback(feedBackConstant)
   }
 }
+
+@RequiresApi(Build.VERSION_CODES.R)
+@Retention(AnnotationRetention.SOURCE)
+@Target(AnnotationTarget.VALUE_PARAMETER)
+@IntDef(
+  CONTEXT_CLICK,
+  CLOCK_TICK,
+  VIRTUAL_KEY,
+  // KEYBOARD_TAP, // KEYBOARD_PRESS と値が重複するためコメントアウト
+  LONG_PRESS,
+  KEYBOARD_PRESS,
+  KEYBOARD_RELEASE,
+  TEXT_HANDLE_MOVE,
+  VIRTUAL_KEY_RELEASE,
+  CONFIRM,
+  REJECT,
+  GESTURE_START,
+  GESTURE_END,
+)
+private annotation class FeedBackConstant
 
 @Preview
 @Composable
